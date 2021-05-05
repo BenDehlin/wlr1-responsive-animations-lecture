6d2d692bef666f6c27620a6e104057fbc1d0f4e4
BenDehlin <ben.dehlin@gmail.com>
2021-05-05 13:33:04 -0600

    animations lecture

---
+++ src/components/Example.js
@@ -0,0 +1,24 @@
+import {Component} from 'react'
+
+class Example extends Component{
+    constructor(){
+        super()
+        this.state = {
+            spinning: true
+        }
+    }
+
+    animate = () => {
+        this.setState({spinning: !this.state.spinning})
+    }
+
+    render(){
+        return(
+            <div className="big-box">
+                <div onClick={this.animate} className={`small-box-three ${this.state.spinning ? 'pause': ''}`}></div>
+            </div>
+        )
+    }
+}
+
+export default Example
