1eafd136fc16dcdfe715c47dcccc63a07ee3bf78
BenDehlin <ben.dehlin@gmail.com>
2021-05-05 13:56:15 -0600

    notes added

--- src/App.js
+++ src/App.js
@@ -2,15 +2,28 @@ import './App.css';
 import Example from './components/Example'
 
 function App() {
+  // Each of our examples below is contained inside of a "big-box"
+  // so that we can see the animation happening inside the container
   return (
     <div className="App">
       <div className="big-box">
+        {/* Our first example is an example of a simple transition.
+        if you go look at the styling of .small-box in our
+        css you can see specifically what it's doing. */}
         <div className="small-box"></div>
       </div>
       <div className="big-box">
+        {/* Our second example is a slightly complicated animation
+        where the animated item starts in the top left, moves in
+        a circle around the container, turns from a square to a
+        circle and back, and spins slowly. To look at how we animated
+        this take a look at the css for small-box-two as well as
+        the my-animations keyframes. */}
         <div className="small-box-two">
         </div>
       </div>
+      {/* This last box needed some state so we moved it into its
+      own component. Check out ./components/Example.js for that */}
       <Example />
     </div>
   );
--- src/components/Example.js
+++ src/components/Example.js
@@ -4,18 +4,34 @@ class Example extends Component{
     constructor(){
         super()
         this.state = {
+            // this is our piece of state that decides whether or
+            // not the .pause class is applied to our box.
             spinning: true
         }
     }
 
+    // this functions toggles us in and out of the spinning state
+    // so we can alternate.
     animate = () => {
         this.setState({spinning: !this.state.spinning})
     }
 
     render(){
         return(
+            // notice our containing div has the big-box class still
             <div className="big-box">
-                <div onClick={this.animate} className={`small-box-three ${this.state.spinning ? 'pause': ''}`}></div>
+                {/* our item inside of the container will always have the styling
+                of .small-box-three which gives it a size/color as well
+                as the spin keyframes animation. This animation will be
+                going by default but if we click the box it will set the
+                state of spinning to be false. When that happens our box
+                will conditionally be given a second className. This className
+                sets the animation-play-state to be paused so it will stop
+                mid-animation. If we want to start it again we simply click
+                the box again and it will remove the pause class from our item. */}
+                <div 
+                onClick={this.animate} 
+                className={`small-box-three ${this.state.spinning ? 'pause': ''}`}></div>
             </div>
         )
     }
